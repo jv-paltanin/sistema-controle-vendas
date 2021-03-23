9cdffc32fad0c5644d51153f2046c7f5b55a34d3
João Victor <joaovpaltanin116@gmail.com>
2021-03-23 10:55:45 -0300

    Add files via upload

---
+++ Insert Regiões.sql
@@ -0,0 +1,5 @@
+Insert into regiao (id_regiao, nome) values (1, 'Norte');
+Insert into regiao (id_regiao, nome) values (2, 'Nordeste');
+Insert into regiao (id_regiao, nome) values (3, 'Sudeste');
+Insert into regiao (id_regiao, nome) values (4, 'Sul');
+Insert into regiao (id_regiao, nome) values (5, 'Centro-Oeste');

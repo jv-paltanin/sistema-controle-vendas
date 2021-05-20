c4ef40a8c8f1f20fe1a82489c9b9d6b1b6717bc5
João Victor <joaovpaltanin116@gmail.com>
2021-05-20 01:20:30 -0300

    Delete Insert Regiões.sql

--- Insert Regiões.sql
+++
@@ -1,5 +0,0 @@
-Insert into regiao (id_regiao, nome) values (1, 'Norte');
-Insert into regiao (id_regiao, nome) values (2, 'Nordeste');
-Insert into regiao (id_regiao, nome) values (3, 'Sudeste');
-Insert into regiao (id_regiao, nome) values (4, 'Sul');
-Insert into regiao (id_regiao, nome) values (5, 'Centro-Oeste');
